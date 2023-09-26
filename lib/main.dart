866957c468c287c9b91ee5101c3c52abe8182bf6
jacob-macleod <jacob.e.macleod@gmail.com>
2023-09-26 17:45:41 +0100

    Added a gradient background widget

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       home: Scaffold(
-        body: GradientBackground(),
+        body: GradientBackground(
+          widget: Text("Hello World"),
+        ),
       ),
     );
   }
--- lib/widgets/gradient_background.dart
+++ lib/widgets/gradient_background.dart
@@ -1,11 +1,20 @@
 import 'package:flutter/material.dart';
 
 class GradientBackground extends StatelessWidget {
-  const GradientBackground({super.key});
+  const GradientBackground({required this.widget, super.key});
+  final Widget widget;
 
   @override
   Widget build(context) {
-    return const Text(
-        "Hello, world! This is a message from the GradientBackground widget.");
+    return Container(
+      width: double.infinity,
+      height: double.infinity,
+      decoration: const BoxDecoration(
+          gradient: LinearGradient(colors: [
+        Color.fromRGBO(39, 39, 39, 1),
+        Color.fromRGBO(46, 45, 49, 1),
+      ], begin: Alignment.topLeft, end: Alignment.bottomRight)),
+      child: widget,
+    );
   }
 }
