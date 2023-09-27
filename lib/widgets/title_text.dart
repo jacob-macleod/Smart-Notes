eaa0ecb912312535e40e91f858f136467cbea9ff
jacob-macleod <jacob.e.macleod@gmail.com>
2023-09-27 08:14:44 +0100

    Updated title widget

--- lib/widgets/title_text.dart
+++ lib/widgets/title_text.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
 
 class TitleText extends StatelessWidget {
-  const TitleText({super.key});
+  const TitleText({super.key, this.text});
+  final String text;
 
   @override
   Widget build(context) {
     return Text(
-      "Smart Notes",
+      text,
       style: Theme.of(context).textTheme.displayLarge,
     );
   }
