b3e0e9d8b33ae72c57b7d47ff6339f1c17c818f7
jacob-macleod <jacob.e.macleod@gmail.com>
2023-10-26 20:03:35 +0100

    Added code to get all notes

--- lib/notes.dart
+++ lib/notes.dart
@@ -1,18 +1,35 @@
 import 'package:hive/hive.dart';
 import 'package:flutter/material.dart';
 
-void DeleteAllNotes(var box) {
+void deleteAllNotes(var box) {
   var my_box = Hive.box("notes");
   my_box.put(1, "");
 }
 
   // Notes are stored as &&START_NOTE&&Title&&SEPERATOR&&body$$START_NOTE$$...
-  void SaveNote(String title, String body, var box) async {
+  void saveNote(String title, String body, var box) async {
     var my_box = Hive.box("notes");
     String notes = my_box.get(1);
     notes = "$notes&&START_NOTE&&$title&&SEPERATOR&&$body";
     my_box.put(1, notes);
-
-    print ("Notes are now");
-    print (my_box.get(1));
   }
+
+List<List<String>> getAllNotes(var box) {
+  print ("Getting all notes");
+  var my_box = Hive.box("notes");
+  String all_notes = my_box.get(1);
+  List<String> notes = all_notes.split("&&START_NOTE&&");
+  List<List<String>> noteData = [["", ""]];
+
+  for (int i=0; i < notes.length; i++) {
+    List<String> noteContent = notes[i].split("&&SEPERATOR&&");
+    if (noteContent.length == 2) {
+      noteData.add(noteContent);
+    } else {
+      print ("Note content is ");
+      print (noteContent);
+    }
+  }
+
+  return noteData;
+}
--- lib/screens/main_page.dart
+++ lib/screens/main_page.dart
@@ -11,11 +11,11 @@ class MainPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // Generate demo notes - should run once!
-    List notes = [];
-    DeleteAllNotes(box);
-    SaveNote("my title", "my body", box);
-    SaveNote("my title2", "my body2", box);
-    SaveNote("my title3", "my body3", box);
+    deleteAllNotes(box);
+    saveNote("my title", "my body", box);
+    saveNote("my title2", "my body2", box);
+    saveNote("my title3", "my body3", box);
+    List<List<String>> notes = getAllNotes(box);
 
     return GradientBackground(
       widget: Column(
