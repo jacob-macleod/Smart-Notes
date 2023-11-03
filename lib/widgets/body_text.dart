e3ec33a84cb7315de0d15f4fa5a459a1187f67a6
jacob-macleod <jacob.e.macleod@gmail.com>
2023-11-03 19:56:58 +0000

    Made note text editable

--- lib/widgets/body_text.dart
+++ lib/widgets/body_text.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
+/*
 class Body extends StatelessWidget {
   const Body({super.key, required this.text});
   final String text;
@@ -17,6 +19,36 @@ class Body extends StatelessWidget {
               color: Color.fromRGBO(201, 201, 201, 1),
               fontSize: 20,
               fontWeight: FontWeight.bold),
+        ),
+      ),
+    );
+  }
+}*/
+
+class Body extends StatelessWidget {
+  const Body({super.key, required this.text});
+  final text;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: 250,
+      child: TextFormField(
+        onChanged: (data) {
+          print ("Changed to" + data);
+        },
+        initialValue: text,
+        maxLines: 20,
+        minLines: 10,
+        autocorrect: true,
+        style: const TextStyle(
+              backgroundColor: Colors.transparent,
+              color: Color.fromRGBO(201, 201, 201, 1),
+              fontSize: 20,
+              fontWeight: FontWeight.bold),
+        decoration: const InputDecoration(
+          border: OutlineInputBorder(),
+          labelText: 'Note',
         ),
       ),
     );
